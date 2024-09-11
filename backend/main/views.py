c21b5fc81632c1908d85f8ce7b0de823d7002e19
followthepanda1 <167018262+followthepanda1@users.noreply.github.com>
2024-09-11 15:22:28 +0300

    основа для странички

--- backend/main/views.py
+++ backend/main/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
 # Create your views here.
+class Chat(APIView):
+    def post(self, request):
+        data = request.data
+        return Response({'title': 'Чат-бот'})
